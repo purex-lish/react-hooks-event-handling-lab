7eb505ab3155ad9f7f45a79b438acaa00cc1c72e
Purity Wambua <puritywamb@gmail.com>
2024-08-06 23:21:14 +0300

    react event

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
-
 function Keypad (){
+    function handleChange(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange} name="passwordis: "placeholder="enter password"></input>
+        </div>
     )
 }
 
